1bc21f2a169096f5c318f520cdf061553eccbbfa
sagar seth <sagar12jan@gmail.com>
2018-03-19 19:03:07 +0530

    resolve bug in web model

--- app/WebModel.php
+++ app/WebModel.php
@@ -11,7 +11,7 @@ class WebModel extends Model
     public function getHomeData()
     {
      $resources  = DB::table('gs_resources')->select('id','title', 'summary','image')->where('status', '1')->limit(8)->get();
-     $job 		 = DB::table('gs_jobInfo')->select('id','title', 'description','image','organisation_name','org_city')->where('publish', '1')->limit(8)->get();
+     $job = DB::table('gs_jobInfo')->select('id','title', 'description','image','organisation_name','org_city')->where('publish', '1')->limit(8)->get();
      $event 	 = DB::table('gs_eventinfo')->select('id','name', 'description','image','sport_name','organizer_city','start_date','end_date','entry_start_date','entry_end_date')->where('publish', '1')->limit(8)->get();
      $tournament = DB::table('gs_tournament_info')->select('id','name', 'image','start_date','end_date','event_entry_date','event_end_date','org_city','sport')->where('publish', '1')->limit(8)->get();
 	$data =  array('resources' => $resources,'job'=>$job,'event'=>$event,'tournament'=>$tournament );
@@ -61,11 +61,7 @@ class WebModel extends Model
 
  public function getJobDetail($id)
     {
-<<<<<<< HEAD
-        $job         = DB::table('gs_jobInfo')->select('id','title', 'description','image','organisation_name','org_city','gender','type','type','work_experience','organisation_name','about','address1','org_city','pin','job_link','sport')->where('id', $id)->get();
-=======
         $job         = DB::table('gs_jobInfo')->select('title', 'description','image','organisation_name','org_city','gender','type','type','work_experience','organisation_name','about','address1','org_city','pin','job_link','sport','salary')->where('id', $id)->get();
->>>>>>> 87548af3306652dfbfd6c57d1678aee562af0605
 
      return $job;
     }
@@ -73,14 +69,14 @@ class WebModel extends Model
 
  public function getTournamentDetail($id)
  {
- $tournament = DB::table('gs_tournament_info')->select('id','name', 'image','start_date','end_date','event_entry_date','event_end_date','org_city','sport','address_1','description','tournaments_link','org_city','org_pin','organiser_name')->where('id',$id)->get();
+ $tournament = DB::table('gs_tournament_info')->select('name', 'image','start_date','end_date','event_entry_date','event_end_date','org_city','sport','address_1','description','tournaments_link','org_city','org_pin','organiser_name')->where('id',$id)->get();
      return $tournament;
  }
 
 
  public function getArticleDetail($id)
     {
-        $resources  = DB::table('gs_resources')->select('id','title', 'summary','image','description','date_created','url','location','topic_of_artical')->where('id', $id)->get();
+        $resources  = DB::table('gs_resources')->select('title', 'summary','image','description','date_created','url','location','topic_of_artical')->where('id', $id)->get();
      return $resources ;
 
     }
@@ -89,7 +85,7 @@ class WebModel extends Model
 
 public function  getEventDetail_data($sport_name)
 {
-     $event      = DB::table('gs_eventinfo')->select('id','name','type', 'description','image','sport_name','organizer_city','start_date','end_date','entry_start_date','entry_end_date','pin','address_1','organizer_name','email_app_collection','event_links')->where('sport_name', $sport_name)->get();
+     $event      = DB::table('gs_eventinfo')->select('name','type', 'description','image','sport_name','organizer_city','start_date','end_date','entry_start_date','entry_end_date','pin','address_1','organizer_name','email_app_collection','event_links')->where('sport_name', $sport_name)->get();
      return $event;
 
 
