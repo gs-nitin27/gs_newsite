38bb5749a8f0db48d03864ca315b51db32eb89ef
sagar seth <sagar12jan@gmail.com>
2018-03-27 17:37:43 +0530

    start redirection

--- resources/views/Manage/layouts/header.blade.php
+++ resources/views/Manage/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <?php $value = session('userdata');
-echo "dsdsssd";$sess_data = $value;die;
+echo "dsdsssd";$sess_data = $value;print_r($sess_data);die;
 $userid = $sess_data->userid;
 $sess_name = $sess_data->name;
 $sess_email = $sess_data->email;
