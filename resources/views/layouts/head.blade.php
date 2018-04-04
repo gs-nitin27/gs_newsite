a4cffe0f74453955283bec8b51f0f2757c8e127b
sagar seth <sagar12jan@gmail.com>
2018-04-04 14:24:21 +0530

    added js

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,8 +1,6 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
         @yield('meta')
-        <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
-<script src="https://npmcdn.com/bootstrap@4.0.0-alpha.5/dist/js/bootstrap.min.js"></script>
         <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
         <link href="{{asset('css/style.css')}}" rel="stylesheet">
         <link href="{{asset('css/media.css')}}" rel="stylesheet">
@@ -15,3 +13,5 @@
     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
     <script src="{{asset('js/owl.carousel.js')}}"></script>
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+          <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+<script src="https://npmcdn.com/bootstrap@4.0.0-alpha.5/dist/js/bootstrap.min.js"></script>
