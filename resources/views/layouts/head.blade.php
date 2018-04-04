db9a541d46e58d6d9e9e3fc77f396328d24182f5
sagar seth <sagar12jan@gmail.com>
2018-04-04 15:50:41 +0530

    layout improvement

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,23 +1,5 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+'GTM-N3SWFF5';
-})(window,document,'script','dataLayer','GTM-N3SWFF5');</script>
-
-<!-- End Google Tag Manager -->
-    <!-- Required meta tags always come first -->
-<meta charset="utf-8">
-
-<meta name="description" content="
-We bring you the latest updates on all the big sports Events & Tournaments to your GetSporty App. Also, Helps you to Find your dream sports quota jobs in India.">
-
-<meta name="google-site-verification" content="1Lz4Ejk_OiCgTSYN7eBGPARofMMt7GyKbJ55yAKzzNs" />
-<meta name="google-site-verification" content="RjBA1hezj08g12vWgRFMJjRoPJTTSZ86ubx2Qvo5nig" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="keywords" content="Tournaments, Events, Jobs, Coaches ">
         @yield('meta')
         <link href="{{asset('public/css/bootstrap.min.css')}}" rel="stylesheet">
         <link href="{{asset('public/css/style1.css')}}" rel="stylesheet">
