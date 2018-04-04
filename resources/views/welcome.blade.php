a60a8a6a0445ae450e3365cff94c7937fde53ba5
sagar seth <sagar12jan@gmail.com>
2018-04-04 17:07:30 +0530

    added utube vidwo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -167,7 +167,7 @@ box-shadow: rgba(255,255,255, 0.75) 1.5em 0 0 0, rgba(255,255,255, 0.75) 1.1em 1
                          <button type="button" class="close" data-dismiss="modal">&times;</button>
 
 
-                          <iframe width="100%" height="315" src="https://www.youtube.com/embed/6QsIvzS4_ws" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                          <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=bVcfL6KtFfs&feature=youtu.be" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 
                           <!-- <iframe width="100%" height="315" src="https://www.youtube.com/embed/njooQXjHmb0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
