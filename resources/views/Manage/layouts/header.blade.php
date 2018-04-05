b16931ca26e9de9bcaaca25b4fac3f4a5e109d14
sagar seth <sagar12jan@gmail.com>
2018-04-05 18:34:28 +0530

    added app download link

--- resources/views/Manage/layouts/header.blade.php
+++ resources/views/Manage/layouts/header.blade.php
@@ -56,6 +56,7 @@ $org_id = '';
                     <li><a href="{{url('manage/my_profile/')}}<?php echo '/'.$crt_user_id; ?>"><i class="glyphicons glyphicons-user pull-right close_mySidenav"></i>{{$sess_name}}</a></li>
                     <li><a href="#"><i class="fa fa-times pull-right close_mySidenav"></i></a></li>
                     <li><a href="{{url('/manage/job/transaction_list')}}"><i class="fa fa-inr"></i>My transaction</a></li>
+                    <li><a href="javascript:void(0)"><i class="fa fa-mobile"></i>Get App</a></li>
                     <!-- <li><a href="#"><i class="fa fa-briefcase"></i>Job</a></li>
                     <li><a href="#"><i class="fa fa-bell"></i>Notification</a></li> -->
                     <li><a href="{{url('/manage/logout')}}" onclick="window.localStorage.clear();"><i class="fa fa-power-off"></i>Logout</a></li>
