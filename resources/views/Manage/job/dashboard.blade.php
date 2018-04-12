030f26f6a9fd8f1b9f868c8199684102f2811e4f
sagar seth <sagar12jan@gmail.com>
2018-04-12 18:42:08 +0530

    add org

--- resources/views/Manage/job/dashboard.blade.php
+++ resources/views/Manage/job/dashboard.blade.php
@@ -335,8 +335,10 @@
              $('#org_div').html('');
              $('#org_div').html(org_name);
              var userdata = JSON.parse(localStorage.getItem('userdata'));
-             userdata.org_data.org_id = resp_data;
-             userdata.org_data.org_name = $('#org_name').val();
+             var obj = {};
+             obj["org_id"] = resp_data;
+             obj["org_name"] = $('#org_name').val();
+             userdata.org_data.push(obj);
              localStorage.setItem('userdata',JSON.stringify(userdata));
              $('.modal').modal('hide');
            }
