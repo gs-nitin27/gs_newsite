94f522e45554b71546893a9a8fa755238f2a6d98
sagar seth <sagar12jan@gmail.com>
2018-04-16 12:16:18 +0530

    added the note

--- resources/views/article-detail.blade.php
+++ resources/views/article-detail.blade.php
@@ -86,7 +86,10 @@
         	         <hr>
         	         <h4></h4>
         	         <p>{!!$name[0]->description!!}</p>
-        	        </div>       	     
+        	        </div>
+                  <div>
+  <h4>Note:</h4><p>GetSporty is not an organizer for this event / tournament and has no control over any changes in the above given information. For any info. Please connect to the above given contact details.</p>
+</div>       	     
              	  </div>
                 </div> 
 
--- resources/views/job-listing-detail.blade.php
+++ resources/views/job-listing-detail.blade.php
@@ -104,6 +104,9 @@
         			  
         			   <p>For more info : <span class="read-c"><a href="{{$name[0]->job_link}}"> Click here</a> </span></p>
         			    </div>
+                  <div>
+  <h4>Note:</h4><p>GetSporty is not an organizer for this event / tournament and has no control over any changes in the above given information. For any info. Please connect to the above given contact details.</p>
+</div>
         		      </div>
         		    </div>
   	 <div class="clearfix margin20"></div>
