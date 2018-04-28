7ab1756c399c02cac0d29c158b757fa5aa6ac6af
nitn <nitin@darkhorsesports.in>
2018-04-29 00:38:47 +0530

    added meteta tags

--- resources/views/vacancy_view.blade.php
+++ resources/views/vacancy_view.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
 @section('pageTitle','Careers')
+@section('meta')
+<meta name="description" content="UI/UX Designer required , with good experience in html,css and Photoshop">
+<meta name="keywords" content="ui designer, HTML , CSS , web Designer job vacancy , ui designer, HTML developer">
+@endsection
 @section('content')
 <style type="text/css">
 	body{background: #000}
