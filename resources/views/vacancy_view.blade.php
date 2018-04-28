a0569705874abd9cf42a6a21c1148635bec73b52
nitn <nitin@darkhorsesports.in>
2018-04-29 00:45:02 +0530

    added image for ui/ix

--- resources/views/vacancy_view.blade.php
+++ resources/views/vacancy_view.blade.php
@@ -3,6 +3,7 @@
 @section('meta')
 <meta name="description" content="UI/UX Designer required , with good experience in html,css and Photoshop">
 <meta name="keywords" content="ui designer, HTML , CSS , web Designer job vacancy , ui designer, HTML developer">
+<meta property="og:image" content="https://careerfoundry.com/en/blog/difference-between-ux-ui-ux-ui-min-6cd3a171087987d31ecdb67ad67c0ab8136242f7b4e476a0e509017aa8bbc755.png">
 @endsection
 @section('content')
 <style type="text/css">
