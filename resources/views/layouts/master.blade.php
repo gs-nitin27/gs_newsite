c2ea59d009bb41513f44fca80a50dccadfe22286
sagar seth <sagar12jan@gmail.com>
2018-05-07 15:22:36 +0530

    testing env

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <?php
 //die($_SERVER['REMOTE_ADDR'].env('APP_ENV'));die;
 if (env('APP_ENV') === 'testing') {
-if($_SERVER['REMOTE_ADDR'] != '182.69.203.174' || $_SERVER['REMOTE_ADDR'] != '::1')
+if($_SERVER['REMOTE_ADDR'] !== '182.69.203.174' || $_SERVER['REMOTE_ADDR'] !== '::1')
 {
  Redirect::to('https://getsporty.in')->send();
 ?> 
