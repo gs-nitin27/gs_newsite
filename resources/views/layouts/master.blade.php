b094add69a33f31b0824d56d12111005f71ba9f5
sagar seth <sagar12jan@gmail.com>
2018-05-07 15:16:08 +0530

    testing env

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <?php
-//die($_SERVER['REMOTE_ADDR']);die;
+die($_SERVER['REMOTE_ADDR']);die;
 if (env('APP_ENV') === 'testing') {
 if($_SERVER['REMOTE_ADDR'] != '182.69.203.174' || $_SERVER['REMOTE_ADDR'] != '::1')
 {
