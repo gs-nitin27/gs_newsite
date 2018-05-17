ef890f55a94d3cf46da60d64dde018485c6fc3ce
sagar seth <sagar12jan@gmail.com>
2018-05-17 11:23:41 +0530

    added salt velue at checkout

--- resources/views/Manage/job/checkout.blade.php
+++ resources/views/Manage/job/checkout.blade.php
@@ -93,7 +93,7 @@ $sess_data = json_decode($value);
 
                                         <table class="table table-striped" style="font-weight: bold;">
                                         <form method="post" name="payuForm" action="{{ config('constant.FORM_PAYU_URL')}}">
-       <input type="hidden" name="key" id="key" value="{{ config('constant.KEY')}}" />
+       <input type="hidden" name="key" id="key" value="2g3RdB" />
        <input type="hidden" name="hash_string" id="hash_string" value="" />
        <input type="hidden" name="hash"  id="hash" value=""/>
        <input type="hidden" name="txnid" id="txnid" value="" />
