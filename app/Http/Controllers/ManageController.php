3ec2be21e7b5b71f339d1f07a1e53d440e146562
sagar seth <sagar12jan@gmail.com>
2018-05-17 11:27:30 +0530

    added salt value

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -69,8 +69,8 @@ public function create_hash()
   $txnid = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
   $data->txnid = $txnid;
   unset($data->hash);
-  $SALT = config('constant.SALT');//'e5iIg1jwi8';
-    $hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
+  $SALT = 'AwGMsoxe';//config('constant.SALT');//'e5iIg1jwi8';
+  $hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
 
   
      $hashVarsSeq = explode('|', $hashSequence);
