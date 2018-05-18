fa82455fa9cd55d2708cf77c47d7430fe2cca966
sagar seth <sagar12jan@gmail.com>
2018-05-18 18:36:29 +0530

    added talk to in job alogin

--- resources/views/Manage/layouts/footer.blade.php
+++ resources/views/Manage/layouts/footer.blade.php
@@ -33,3 +33,21 @@
     
 
   </script>
+  <?php
+if (env('APP_ENV') === 'production') {
+?>
+  
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ab1e9f2d7591465c708c175/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
+<?php }?>
