1cdeb4739448f66070df426130f7b63b2ed2f71b
sagar seth <sagar12jan@gmail.com>
2018-05-28 15:16:33 +0530

    added new ip adress

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <?php
 //die($_SERVER['REMOTE_ADDR'].env('APP_ENV'));die;
 if (env('APP_ENV') === 'testing') {
-if($_SERVER['REMOTE_ADDR'] == '171.61.135.200' || $_SERVER['REMOTE_ADDR'] == '::1')  // Check fr testing environment
+if($_SERVER['REMOTE_ADDR'] == '122.177.103.232' || $_SERVER['REMOTE_ADDR'] == '::1')  // Check fr testing environment
 {
 
 ?> 
