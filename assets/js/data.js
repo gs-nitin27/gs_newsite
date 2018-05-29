123a26885c1298dfb388e02deb0da81b44f4dfb2
sagar seth <sagar12jan@gmail.com>
2018-05-29 18:16:43 +0530

    url for api

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,7 +8,7 @@ $(id).empty();
         var settings = $.extend({ 
             loading_gif_url : sec+"://www.getsporty.in/staging/uploads/profile/ajax-loader.gif", //url to loading gif
             end_record_text : 'No more records found!', //no more records to load
-            data_url        : 'http://localhost/testingapp/website_controller.php?act='+module, //url to PHP page
+            data_url        : 'https://getsporty.in/testingapp/website_controller.php?act='+module, //url to PHP page
             start_page      : 1 //initial page
         }, options);
         
