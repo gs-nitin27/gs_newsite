16863c1321cd257b93ab274c3c9160a6d98ef209
nitn <nitin@darkhorsesports.in>
2018-06-03 21:15:07 +0530

    added the meta tag

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,5 +1,6 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="FOOTBALL Trials in Delhi NCR, Football summer camps , Football trials">
         @yield('meta')
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
         <meta name="google-site-verification" content="1Lz4Ejk_OiCgTSYN7eBGPARofMMt7GyKbJ55yAKzzNs" />
