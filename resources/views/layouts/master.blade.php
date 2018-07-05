87ec96519bc62134c700f7ebd54fe2cf8d650093
sagar seth <sagar12jan@gmail.com>
2018-07-05 11:48:04 +0530

    event pass download functionality under process

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <?php
 //die($_SERVER['REMOTE_ADDR'].env('APP_ENV'));die;
 if (env('APP_ENV') === 'testing') {
-if($_SERVER['REMOTE_ADDR'] == '122.177.29.59' || $_SERVER['REMOTE_ADDR'] == '::1'|| $_SERVER['REMOTE_ADDR'] == '116.75.228.87' || $_SERVER['REMOTE_ADDR'] == '47.30.5.67')  // Check fr testing environment
+if($_SERVER['REMOTE_ADDR'] == '122.177.86.158' || $_SERVER['REMOTE_ADDR'] == '::1'|| $_SERVER['REMOTE_ADDR'] == '116.75.228.87' || $_SERVER['REMOTE_ADDR'] == '47.30.5.67')  // Check fr testing environment
 {
 
 ?> 
